0aff5c855794860bca81b0f77bad3f8111976c9d
Mohamed Jameel <jameelopix@live.in>
2019-07-30 10:28:06 +0530

    labur rae screen addedd

--- src/app/labour-rate/labour-rate.component.ts
+++ src/app/labour-rate/labour-rate.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { LabourRateService } from "../services/labour-rate.service";
+import { MessageUtilService } from "../services/message-util.service";
+import { MESSAGES } from "../model/messages";
+import { UIService } from "../services/ui.service";
+import { ProjectService } from "../services/project.service";
+import { ElementTypeService } from "../services/element-type.service";
 
 @Component({
   selector: 'app-labour-rate',
@@ -6,10 +13,254 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./labour-rate.component.css']
 })
 export class LabourRateComponent implements OnInit {
+  labourRateDTOList: any[] = [];
+  labourRateDialogVisibilty: boolean = false;
+  labourRateForm: FormGroup;
+  labourRateSearchForm: FormGroup;
 
-  constructor() { }
+  projectDTOList: any[] = [];
+  elementTypeDTOList: any[] = [];
 
-  ngOnInit() {
+  messages: any[] = [];
+
+  constructor(
+    private labourRateService: LabourRateService,
+    private projectService: ProjectService,
+    private elementTypeService: ElementTypeService,
+    private messageUtilService: MessageUtilService,
+    private uiservice: UIService,
+    private formBuilder: FormBuilder
+  ) {
+    this.createFormGroup();
   }
 
+  ngOnInit() {
+    this.fetchReleventData();
+  }
+
+  fetchReleventData() {
+    this.getProjectDTOList();
+  }
+
+  getProjectDTOList() {
+    this.projectService.get({}, (response: any) => {
+      this.projectDTOList = response["projectDTOList"];
+    });
+  }
+
+  onProjectChange(event: any) {
+    let value = event["value"];
+    // console.log(JSON.stringify(value));
+
+    let request = {
+      elementTypeSearchDTO: {
+        projectIdList: [value["id"]]
+      }
+    };
+    this.elementTypeService.get(request, (response: any) => {
+      this.elementTypeDTOList = response.elementTypeDTOList;
+    });
+  }
+
+  createFormGroup() {
+    this.labourRateForm = this.formBuilder.group({
+      id: [null],
+      workType: ["", Validators.required],
+      workDesc: [""],
+      rate: ["", Validators.required],
+      unit: ["", Validators.required],
+      projectId: ["", Validators.required],
+      elementTypeId: ["", Validators.required]
+    });
+
+    this.labourRateSearchForm = this.formBuilder.group({
+      workTypeList: [""],
+      workDescList: [""],
+      rateList: [""],
+      unitList: [""],
+      projectIdList: [""],
+      elementTypeIdList: [""]
+    });
+  }
+
+  addLabourRate = () => {
+    this.labourRateForm.reset();
+    this.labourRateDialogVisibilty = true;
+  };
+
+  editLabourRate = rowData => {
+    this.populateLabourRateForm(rowData, false);
+    this.labourRateDialogVisibilty = true;
+  };
+
+  copyLabourRate = rowData => {
+    this.populateLabourRateForm(rowData);
+    this.labourRateDialogVisibilty = true;
+  };
+
+  populateLabourRateForm(rowData: any, makeIdNull: boolean = true) {
+    this.labourRateForm.reset();
+
+    let id = null;
+    if (!makeIdNull) {
+      id = rowData["id"];
+    }
+
+    let formValue = {
+      id: id,
+      workType: { id: rowData["workType"] },
+      projectId: { id: rowData["projectId"] },
+      elementTypeId: { id: rowData["elementTypeId"] },
+      unit: { id: rowData["unit"] },
+      rate: rowData["rate"],
+      workDesc: rowData["workDesc"]
+    };
+
+    this.labourRateForm.setValue(formValue);
+  }
+
+  saveLabourRate = () => {
+    this.messageUtilService.clearMessage(this.messages);
+    let formData = this.labourRateForm.getRawValue();
+
+    let request = {
+      labourRateDTO: {
+        id: formData["id"],
+        workType: formData["workType"]["id"],
+        projectId: formData["projectId"]["id"],
+        elementTypeId: formData["elementTypeId"]["id"],
+        unit: formData["unit"]["id"],
+        rate: formData["rate"],
+        workDesc: formData["workDesc"]
+      }
+    };
+
+    this.labourRateService.save(
+      request,
+      this.saveLabourRateSuccessCallback,
+      this.saveLabourRateErrorCallback
+    );
+  };
+
+  saveLabourRateSuccessCallback = (response: any) => {
+    this.messageUtilService.showSuccessMessages(
+      this.messages,
+      MESSAGES.SAVE_ACK_MSG
+    );
+    this.labourRateDialogVisibilty = false;
+    this.getLabourRate();
+  };
+
+  saveLabourRateErrorCallback = (response: any) => {
+    this.messageUtilService.showErrorMessages(this.messages, response[0]);
+  };
+
+  cancelLabourRate = () => {
+    this.labourRateForm.reset();
+    this.labourRateDialogVisibilty = false;
+  };
+
+  getLabourRate = () => {
+    let searchFormData = this.labourRateSearchForm.getRawValue();
+
+    let request = {
+      labourRateSearchDTO: {
+        workTypeList: this.uiservice.getSearchData(
+          searchFormData,
+          "workTypeList"
+        ),
+        workDescList: this.uiservice.getSearchData(
+          searchFormData,
+          "workDescList"
+        ),
+        rateList: this.uiservice.getSearchData(
+          searchFormData,
+          "rateList"
+        ),
+        unitList: this.uiservice.getSearchData(
+          searchFormData,
+          "unitList"
+        ),
+        projectIdList: this.uiservice.getSearchData(
+          searchFormData,
+          "projectIdList"
+        ),
+        elementTypeIdList: this.uiservice.getSearchData(
+          searchFormData,
+          "elementTypeIdList"
+        ),
+        projectNeeded: true,
+        elementTypeNeeded: true
+      }
+    };
+
+    this.labourRateDTOList = [];
+    this.labourRateService.get(request, this.getLabourRateCallback);
+  };
+
+  getLabourRateCallback = (response: any) => {
+    this.labourRateDTOList = response.labourRateDTOList;
+  };
+
+  deleteLabourRate = rowData => {
+    this.messageUtilService.clearMessage(this.messages);
+
+    this.labourRateService.delete(
+      rowData["id"],
+      this.deleteLabourRateSuccessCallback,
+      this.deleteLabourRateErrorCallback
+    );
+  };
+
+  deleteLabourRateSuccessCallback = (response: any) => {
+    this.messageUtilService.showSuccessMessages(
+      this.messages,
+      MESSAGES.DELETE_ACK_MSG
+    );
+    this.getLabourRate();
+  };
+
+  deleteLabourRateErrorCallback = (response: any) => {
+    this.messageUtilService.showErrorMessages(this.messages, response[0]);
+  };
+
+  labourRateOptions: any = {
+    caption: "LabourRate Details",
+    addCallback: this.addLabourRate,
+    editCallback: this.editLabourRate,
+    copyCallback: this.copyLabourRate,
+    getCallback: this.getLabourRate,
+    deleteCallback: this.deleteLabourRate,
+    childPresent: true,
+    columns: [
+      {
+        name: "Project",
+        index: "projectDTO.name"
+      },
+      {
+        name: "Element Type",
+        index: "elementTypeDTO.name"
+      },
+      {
+        name: "Work Type",
+        index: "workType"
+      },
+      {
+        name: "Description",
+        index: "workDesc"
+      },
+      {
+        name: "Rate",
+        index: "rate"
+      },
+      {
+        name: "Unit",
+        index: "unit"
+      }
+    ]
+  };
+
+  getControl(field: any) {
+    return this.labourRateForm.controls[field];
+  }
 }
--- src/app/sub-contractor/sub-contractor.component.ts
+++ src/app/sub-contractor/sub-contractor.component.ts
@@ -3,8 +3,9 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { SubContractorService } from "../services/sub-contractor.service";
 import { MessageUtilService } from "../services/message-util.service";
 import { MESSAGES } from "../model/messages";
-import { CompanyService } from "../services/company.service";
 import { UIService } from "../services/ui.service";
+import { FinancialDetailService } from "../services/financial-detail.service";
+import { AddressService } from "../services/address.service";
 
 @Component({
   selector: "app-sub-contractor",
@@ -16,6 +17,14 @@ export class SubContractorComponent implements OnInit {
   subContractorDialogVisibilty: boolean = false;
   subContractorForm: FormGroup;
   subContractorSearchForm: FormGroup;
+
+  address: any = {};
+  addressDialogVisibilty: boolean = false;
+  addressForm: FormGroup;
+
+  financialDetail: any[] = [];
+  financialDetailDialogVisibilty: boolean = false;
+  financialDetailForm: FormGroup;
 
   statusList: any[] = [
     {
@@ -34,6 +43,8 @@ export class SubContractorComponent implements OnInit {
 
   constructor(
     private subContractorService: SubContractorService,
+    private financialDetailService: FinancialDetailService,
+    private addressService: AddressService,
     private messageUtilService: MessageUtilService,
     private uiservice: UIService,
     private formBuilder: FormBuilder
@@ -45,7 +56,7 @@ export class SubContractorComponent implements OnInit {
     this.fetchReleventData();
   }
 
-  fetchReleventData() {}
+  fetchReleventData() { }
 
   createFormGroup() {
     this.subContractorForm = this.formBuilder.group({
@@ -57,6 +68,24 @@ export class SubContractorComponent implements OnInit {
     this.subContractorSearchForm = this.formBuilder.group({
       codeList: [""],
       activeList: [""]
+    });
+
+    this.financialDetailForm = this.formBuilder.group({
+      id: [null],
+      gst: [""],
+      pan: [""]
+    });
+
+    this.addressForm = this.formBuilder.group({
+      id: [null],
+      addressLine1: [""],
+      addressLine2: [""],
+      landmark: [""],
+      city: [""],
+      state: [""],
+      country: [""],
+      mobNo: [""],
+      pincode: [""]
     });
   }
 
@@ -90,6 +119,42 @@ export class SubContractorComponent implements OnInit {
     };
 
     this.subContractorForm.setValue(formValue);
+  }
+
+  populateAddressForm(rowData: any) {
+    this.addressForm.reset();
+
+    let formValue = {};
+    if (rowData) {
+      formValue = {
+        id: rowData["id"],
+        addressLine1: rowData["addressLine1"],
+        addressLine2: rowData["addressLine2"],
+        landmark: rowData["landmark"],
+        city: rowData["city"],
+        state: rowData["state"],
+        country: rowData["country"],
+        mobNo: rowData["mobNo"],
+        pincode: rowData["pincode"],
+      };
+    }
+
+    this.addressForm.patchValue(formValue);
+  }
+
+  populateFinancialDetailForm(rowData: any) {
+    this.financialDetailForm.reset();
+
+    let formValue = {};
+    if (rowData) {
+      formValue = {
+        id: rowData["id"],
+        gst: rowData["gst"],
+        pan: rowData["pan"]
+      };
+    }
+
+    this.financialDetailForm.patchValue(formValue);
   }
 
   saveSubContractor = () => {
@@ -169,22 +234,122 @@ export class SubContractorComponent implements OnInit {
     this.messageUtilService.showErrorMessages(this.messages, response[0]);
   };
 
-  getAddress = () => {
-    // let searchFormData = this.subContractorSearchForm.getRawValue();
+  getAddress = rowData => {
+    let addressId = rowData["addressId"];
 
-    // let request = {
-    //   subContractorSearchDTO: {
-    //     codeList: this.uiservice.getSearchData(searchFormData, "codeList"),
-    //     activeList: this.uiservice.getSearchData(searchFormData, "activeList")
-    //   }
-    // };
-
-    // this.subContractorDTOList = [];
-    // this.subContractorService.get(request, this.getSubContractorCallback);
+    if (addressId) {
+      let request = {
+        addressSearchDTO: {
+          idList: addressId
+        }
+      }
+      this.addressService.get(request, this.getAddressSuccessCallback);
+    } else {
+      this.addressDialogVisibilty = true;
+    }
   };
 
-  getFinance = () => {
+  getAddressSuccessCallback = (response: any) => {
+    this.populateAddressForm(response["addressDTOList"][0]);
 
+    this.addressDialogVisibilty = true;
+  };
+
+  getFinance = rowData => {
+    let financialDetailId = rowData["financialDetailId"];
+
+    if (financialDetailId) {
+      let request = {
+        financialDetailSearchDTO: {
+          idList: financialDetailId
+        }
+      }
+      this.financialDetailService.get(request, this.getFinancialDetailSuccessCallback);
+    } else {
+      this.financialDetailDialogVisibilty = true;
+    }
+  };
+
+  getFinancialDetailSuccessCallback = (response: any) => {
+    this.populateFinancialDetailForm(response["financialDetailDTOList"][0]);
+
+    this.financialDetailDialogVisibilty = true;
+  };
+
+  cancelFinancialDetail = () => {
+    this.financialDetailForm.reset();
+    this.financialDetailDialogVisibilty = false;
+  };
+
+  saveFinancialDetail = () => {
+    this.messageUtilService.clearMessage(this.messages);
+    let formData = this.financialDetailForm.getRawValue();
+
+    let request = {
+      financialDetailDTO: {
+        id: formData["id"],
+        gst: formData["gst"],
+        pan: formData["pan"]
+      }
+    };
+
+    this.financialDetailService.save(
+      request,
+      this.saveFinancialDetailSuccessCallback,
+      this.saveFinancialDetailErrorCallback
+    );
+  };
+
+  saveFinancialDetailSuccessCallback = (response: any) => {
+    this.messageUtilService.showSuccessMessages(
+      this.messages,
+      MESSAGES.SAVE_ACK_MSG
+    );
+  };
+
+  saveFinancialDetailErrorCallback = (response: any) => {
+    this.messageUtilService.showErrorMessages(this.messages, response[0]);
+  };
+
+  cancelAddress = () => {
+    this.addressForm.reset();
+    this.addressDialogVisibilty = false;
+  };
+
+  saveAddress = () => {
+    this.messageUtilService.clearMessage(this.messages);
+    let formData = this.addressForm.getRawValue();
+
+    let request = {
+      addressDTO: {
+        id: formData["id"],
+        addressLine1: formData["addressLine1"],
+        addressLine2: formData["addressLine2"],
+        landmark: formData["landmark"],
+        city: formData["city"],
+        state: formData["state"],
+        country: formData["country"],
+        mobNo: formData["mobNo"],
+        pincode: formData["pincode"]
+      }
+    };
+
+    this.addressService.save(
+      request,
+      this.saveAddressSuccessCallback,
+      this.saveAddressErrorCallback
+    );
+  };
+
+  saveAddressSuccessCallback = (response: any) => {
+    this.messageUtilService.showSuccessMessages(
+      this.messages,
+      MESSAGES.SAVE_ACK_MSG
+    );
+  };
+
+  saveAddressErrorCallback = (response: any) => {
+    this.messageUtilService.showErrorMessages(this.messages, response[0]);
   };
 
   actionItems = [
@@ -207,7 +372,7 @@ export class SubContractorComponent implements OnInit {
     copyCallback: this.copySubContractor,
     getCallback: this.getSubContractor,
     deleteCallback: this.deleteSubContractor,
-    childPresent: true,
+    actions: this.actionItems,
     columns: [
       {
         name: "Code",
